afaaf0632f5ec51bff0dc3dfad1a70a139e94ae7
yamtawachi <yam.tawachi@tensorleap.ai>
2025-04-14 09:26:01 +0300

    metrics

--- leap_custom_test.py
+++ leap_custom_test.py
@@ -2,7 +2,7 @@ from code_loader.contract.datasetclasses import SamplePreprocessResponse
 
 from leap_binder import (input_encoder, preprocess_func_leap, gt_encoder,
                          leap_binder, loss, gt_bb_decoder, image_visualizer, bb_decoder,
-                         metadata1, metadata2, iou_dic, cost)
+                         metadata1, metadata2, iou_dic, cost, metadata_metric)
 import tensorflow as tf
 import numpy as np
 from code_loader.helpers import visualize
@@ -31,6 +31,9 @@ def check_custom_test():
             meta_data2=metadata1(idx, subset)
 
             y_pred = model([concat])
+
+            d_metrics=metadata_metric(y_pred[0].numpy(),s_prepro)
+
             iou=iou_dic(y_pred[0].numpy(), s_prepro)
             total_loss=loss(y_pred[1].numpy(),y_pred[2].numpy(),y_pred[3].numpy(),np.expand_dims(gt,axis=0), y_pred[0].numpy())
             cost_dic=cost(y_pred[1].numpy(),y_pred[2].numpy(),y_pred[3].numpy(),np.expand_dims(gt,axis=0))
