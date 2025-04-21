51236b1bc0d34dfaefe645626fa063223cda035f
yamtawachi <yam.tawachi@tensorleap.ai>
2025-04-21 10:31:49 +0300

    fixes

--- ultralytics/tensorleap_folder/global_params.py
+++ ultralytics/tensorleap_folder/global_params.py
@@ -18,9 +18,13 @@ def dict_to_namespace(d):
 def get_yolo_data(cfg):
     from ultralytics.data.utils import check_det_dataset
     return check_det_dataset(cfg.data, autodownload=True)
-def get_criterion(model_path):
+def get_criterion(model_path,cfg):
     from ultralytics import YOLO
     from ultralytics.utils import IterableSimpleNamespace
+    if not model_path.is_absolute():
+        model_path = (cfg.tensorleap_path / model_path).resolve()
+    assert model_path.is_relative_to(cfg.tensorleap_path), (
+        f"❌ {model_path!r} is not inside tensorleap path {cfg.tensorleap_path!r}" )
     model_base = YOLO(model_path)
     criterion = model_base.init_criterion()
     criterion.hyp = IterableSimpleNamespace(**criterion.hyp)
@@ -51,6 +55,6 @@ with open(file_path, 'r') as file:
 cfg = dict_to_namespace(config_dict)
 yolo_data=get_yolo_data(cfg)
 dataset_yaml=get_dataset_yaml(cfg)
-criterion=get_criterion(Path(dataset_yaml['path'])/cfg.model)
+criterion=get_criterion(Path(cfg.model),cfg)
 all_clss=dataset_yaml["names"]
 predictor=get_predictor_obj(cfg,yolo_data)
